8f370c681c092e82b96471ee10732d103a301971
Nikola <nikola.dj.87@gmail.com>
2021-12-26 14:55:01 +0100

    first

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Header from './Header'
-import Menu from './Menu'
-import AboutMe from './AboutMe'
-import Projects from './Projects'
+import React from "react";
+import Header from "./Header";
+import Menu from "./Menu";
+import AboutMe from "./AboutMe";
+// import Projects from "./Projects";
 function Home() {
-    return (
-        <div>
-            <Header />
-            <Menu />
-            <AboutMe />
-            <Projects />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <AboutMe />
+      {/* <Projects /> */}
+    </div>
+  );
 }
 
 export default Home;
